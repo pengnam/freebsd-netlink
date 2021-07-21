2f968713ea57822926a83a4114d7cd830f5645d3
pengnam <seanngpengnam@gmail.com>
2021-07-21 04:59:23 +0000

    Add copy of netlink files from other repository

---
+++ sys/netlink/clean.sh
@@ -0,0 +1,10 @@
+rm .nlsock.c.swp
+rm .nlattr.c.swp
+rm .genetlink.c.swp
+rm .rtnetlink.c.swp
+rm net/.netlink.h.swp
+rm net/.genetlink.h.swp
+rm net/.rtnetlink.h.swp
+rm linux/.netlink.h.swp
+rm linux/.genetlink.h.swp
+rm linux/.rtnetlink.h.swp
